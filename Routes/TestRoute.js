3352dd25f88b707a6c0ae412e5c230df44d8e649
Rock91 <ronak.sutariya90@gmail.com>
2020-01-26 21:49:59 +0530

    test final code

--- Routes/TestRoute.js
+++ Routes/TestRoute.js
@@ -7,7 +7,7 @@ const express = require('express');
 const router = express.Router() 
 
 router.get('/',(req,res)=>{
-    console.log("Test router call..........!");
+    console.log("Test router call...***.......!");
     res.send('Ok')
 }) 
 
